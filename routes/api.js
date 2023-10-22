81e68645c96b8c4d3ee4160c02b4a1f7c27bbe03
yaming <ymideakf@163.com>
2023-10-22 19:20:59 +0800

    测试代码

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,143 @@
 var express = require('express');
 var router = express.Router();
-var {request} = require('./../utils');
+var crypto=require('crypto');
+var {request, ghRequest} = require('./../utils');
+
+/*
+    access_token: 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
+    scope: repo,user
+    token_type: bearer
+*/
+const toBase64 = (str) => {
+    return new Buffer.from(str).toString("base64");
+}
+const sha = (str='') => {
+    var obj = crypto.createHash('sha256');
+    obj.update(str);
+    return obj.digest('hex');
+};
+
+router.get('/gh/authorize', function (req, res, next) {
+    let obj = req.query,
+        code = obj.code || '',
+        state = obj.state || '',
+        client_id = '18c2c94ee9c6ceb11646',
+        client_secret = 'f3714a4cb1c017a2e37218442213a4dd506cf2e2',
+        redirect_uri = 'http://localhost:5713/login',
+        query = (`client_id=${client_id}&client_secret=${client_secret}&code=${code}&redirect_uri=${redirect_uri}`),
+        url = `https://github.com/login/oauth/access_token`;
+    
+    request(url, {method:'post', data: {client_id, client_secret, code, redirect_uri}}).then(response => {
+        res.json(resp(0, response));
+    }).catch(e => {
+        res.json(resp(-1, e));
+    });
+});
+// 获取用户信息，也可以判断用户是否登录
+router.get('/gh/user', function (req, res, next) {
+    ghRequest('/user').then(response => {
+        res.json(resp(0, response));
+    }).catch(e => {
+        res.json(e);
+    });
+});
+// 获取仓库
+router.get('/gh/repo', function (req, res, next) {
+    let owner = req.query.owner,
+        repo = `notem_${owner}`;
+    ghRequest(`/repos/${owner}/${repo}`).then(response => {
+        res.json(resp(0, response));
+    }).catch(e => {
+        reject(repo(-1, e));
+    });
+});
+// 创建仓库
+router.post('/gh/repo', function (req, res, next) {
+    let body = req.body,
+        name = `notem_${body.owner}`,
+        description = body.desc || `我的笔记`;
+
+    if(!(body?.owner || '').trim() || !(body?.desc || '').trim()) {
+        return res.json({code: -1, data: {}, message: '参数异常'});
+    }
+
+    ghRequest(`/user/repos`, {
+        method: 'post',
+        data: {
+            name,
+            description,
+            homepage: '',
+            private: false,
+            is_template: true,
+        }
+    }).then(response => {
+        res.json(resp(0, response));
+    }).catch(e => {
+        reject(repo(-1, e));
+    });
+});
+// 初始化仓库，新增 README.md 文件
+router.put('/gh/init', function (req, res, next) {
+    ghRequest(`/repos/yaming042/notem_yaming042/contents/README.md`, {
+        method: 'put',
+        data: {
+            message: '首次提交',
+            content: toBase64('你好，世界！'),
+        }
+    }).then(response => {
+        res.json(resp(0, response));
+    }).catch(e => {
+        res.json(resp(-1, e));
+    });
+});
+
+// f329913e8eedd90d0295ff62e50e64a0131318f5
+router.get('/gh/test', function (req, res, next) {
+    // ghRequest(`/repos/yaming042/notem_yaming042/git/ref/heads/main`).then(response => {
+    //     res.json(resp(0, response));
+    // }).catch(e => {
+    //     res.json(resp(-1, e));
+    // });
+
+    ghRequest(`/repos/yaming042/notem_yaming042/git/trees/f329913e8eedd90d0295ff62e50e64a0131318f5`).then(response => {
+        res.json(resp(0, response));
+    }).catch(e => {
+        res.json(resp(-1, e));
+    });
+});
+router.post('/gh/dir', function (req, res, next) {
+    ghRequest(`/repos/yaming042/notem_yaming042/contents/note/test.json`, {
+        method: 'put',
+        data: {
+            message: '测试文件夹',
+            content: toBase64('{name:123}'),
+        }
+    }).then(response => {
+        res.json(resp(0, response));
+    }).catch(e => {
+        res.json(resp(-1, e));
+    });
+    
+
+    // let body = req.body;
+
+    // ghRequest(`/repos/yaming042/notem_yaming042/git/trees`, {
+    //     method: 'post',
+    //     data: {
+    //         base_tree: 'f329913e8eedd90d0295ff62e50e64a0131318f5',
+    //         tree: [
+    //             {path: 'scratch', mode: '040000', type: 'tree', content: sha('123')},
+    //         ],
+    //     }
+    // }).then(response => {
+    //     res.json(resp(0, response));
+    // }).catch(e => {
+    //     res.json(resp(-1, e));
+    // });
+});
+
+
+
 
 var access_token = "121.32bed1874d13e147194c8efe33f9f3a8.YGGBcEEzoADiDHk8hrIA437M65C4MIWfM3ZXpJQ.EKA6xA",
     apiPrefix = 'http://pan.baidu.com',
@@ -12,9 +149,9 @@ var resp = (code=0, data={}, message='') => {
         result = JSON.parse(data);
     }catch(e){}
     if(code === 0) {
-        return {code, data: result, message: message || 'success'}
+        return {code, data: result, message: message || '成功'}
     }else{
-        return {code, data: result, message: message || 'Internal Server Error'}
+        return {code, data: result, message: message || '服务器内部错误'}
     }
 };
 
@@ -54,7 +191,7 @@ router.get('/bdyp/validate', function (req, res, next) {
     request(url).then(response => {
         res.json(resp(0, response));
     }).catch(e => {
-        res.json(resp(-1, response));
+        res.json(resp(-1, e));
     });
 });
 
@@ -66,9 +203,37 @@ router.get('/bdyp/list', function (req, res, next) {
     request(url).then(response => {
         res.json(resp(0, response));
     }).catch(e => {
-        res.json(resp(-1, response));
+        res.json(resp(-1, e));
     });
 });
+// 获取文档列表
+router.get('/bdyp/doclist', function (req, res, next) {
+    let token = req.query.access_token || access_token;
+        url = `${apiPrefix}/rest/2.0/xpan/file?method=doclist&access_token=${token}&parent_path=${projectDir}&web=1`;
+
+    request(url).then(response => {
+        let baseUrl = ((response.info || [])[0] || {}).lodocpreview,
+            u = baseUrl + '&part_index=0&method=newinfo';
+
+        res.json(resp(0, response));
+    }).catch(e => {
+        res.json(resp(-1, e));
+    });
+});
+router.get('/bdyp/docinfo', function (req, res, next) {
+    let url = req.query.url;
+    url += '&part_index=0&method=newinfo';
+    console.log(333, url)
+    request(url).then(response => {
+        console.log(222, response)
+        res.json(resp(0, response));
+    }).catch(e => {
+        console.log(111, e.message)
+        res.json(resp(-1, e));
+    });
+});
+
+
 
 // 创建notem文件夹，以后就是笔记的根目录
 router.post('/bdyp/newdir', function (req, res, next) {
@@ -86,7 +251,7 @@ router.post('/bdyp/newdir', function (req, res, next) {
         request(url, options).then(response => {
             res.json(resp(0, response));
         }).catch(e => {
-            res.json(resp(-1, response));
+            res.json(resp(-1, e));
         });
 });
 
--- utils/index.js
+++ utils/index.js
@@ -58,5 +58,61 @@ const request = async (url, options = {}) => {
             });
     });
 };
+const ghRequest = async (url, options = {}) => {
+    let isUrlEncoded = options.contentType === 'application/x-www-form-urlencoded',
+        isFormData = options.contentType === 'multipart/form-data',
+        requestUrl = url;
 
-module.exports = { request };
+    if((!options.method || options.method?.toUpperCase === 'GET') && options.data && Object.keys(options.data).length) {
+        requestUrl += `?${qs.stringify(options.data || {})}`;
+    }
+
+    let ajaxOption = {
+        url: requestUrl,
+        method: options.method || 'GET', // 默认 get
+        baseURL: `https://api.github.com`, // baseURL 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL。
+        headers: {
+            'Content-Type': options.contentType || 'application/json',
+            'Accept': 'application/vnd.github+json',
+            'Authorization': `Bearer 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`,
+            'X-GitHub-Api-Version': '2022-11-28'
+        },
+        data: isUrlEncoded ? qs.stringify(options.data || {}) : (isFormData ? options.data : JSON.stringify(options.data || {})), // 'PUT', 'POST', 和 'PATCH'时body的参数
+        timeout: options.timeout || 60000, // 超时时间 60秒
+        responseType: options.responseType || 'json', // 表示服务器响应的数据类型
+    };
+
+    return new Promise((resolve, reject) => {
+        let defaultError = {code: 101, data: null, message: '请求异常'},
+            responseError = {code: 102, data: null, message: '响应异常'};
+
+        axios(ajaxOption)
+            .then((response) => {
+                // data就是后端接口返回的整体
+                let {data={}, status} = response || {};
+
+                if([200, 201].includes(status)) {
+                    resolve(data);
+                }else{
+                    reject(data);
+                }
+            }).catch(error => {
+                let errorCode = error?.response?.status;
+                if(errorCode === 404) {
+                    reject({code: -1, data: {}, message: '资源不存在'})
+                }else if(errorCode === 301) {
+                    reject({code: -1, data: {}, message: '资源永久移除'})
+                }else if(errorCode === 403) {
+                    reject({code: -1, data: {}, message: '资源权限不足'})
+                }else{
+                    console.log(1, error)
+                    reject(defaultError);
+                }
+            });
+    });
+};
+
+module.exports = {
+    request,
+    ghRequest,
+};
